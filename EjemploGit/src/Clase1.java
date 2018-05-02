1c4d89035afd4e22640ff49f4182255bd90f56c6
Zafiro 03 <Zafiro 03@DESKTOP-N9BA6P3>
2018-05-02 17:08:27 +0200

    Este es el punto inicial del proyecto, solo es un ejemplo

---
+++ EjemploGit/src/Clase1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author Zafiro 03
+ *
+ */
+public class Clase1 {
+	
+	public Clase1() {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
